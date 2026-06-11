26a59b55bf356c3c60bf7af672ebf5510d630229
Lazaro <13026853+LazaroOnline@users.noreply.github.com>
2026-06-12 01:26:25 +0200

    Fix bug when selecting files with the file picker. Add error messages and better logs.

--- hat-converter.js
+++ hat-converter.js
@@ -3,60 +3,123 @@
 Web interface events and listeners
 
 */
-const dlAllLink = document.getElementById("downloadZip")
+const downloadZipButton = document.getElementById("downloadZip")
 
 // handle file "uploads"
-async function handleFiles(filesOrEvent) {
-	dlAllLink.hidden = true;
-	const files = filesOrEvent?.length > 0? filesOrEvent : this.files;
-	const fileList = Array.from(files);
-	console.log(`Started processing ${fileList.length} files in total.`);
-	const promises = [];
+async function handleFiles(files) {
+	try {
+		downloadZipButton.hidden = true;
+		await loadHatFileList(files);
+	}
+	catch(e) {
+		console.error("Error processing files:", e);
+		alert("An error occurred while processing the files. Please check the console for details.");
+	}
+	finally {
+		if (hats.length > 0) {
+			downloadZipButton.hidden = false;
+		}
+		inputElement.value = ''; // Clear the file input for better UX, allowing the same files to be selected again if needed.
+	}
+}
+
+async function loadHatFileList(files) {
 	const BATCH_SIZE = 100;
-	const allResults = [];
-	for (let start = 0; start < fileList.length; start += BATCH_SIZE) {
-		const batch = fileList.slice(start, start + BATCH_SIZE);
-		console.log(`Started processing batch ${batch.length} files...`);
-		const batchPromises = batch.map(file => getHatFileInfo(file));
+	const promises = await batchProcessArray(files, loadHatFile, BATCH_SIZE);
+	const hatPromises = await Promise.all(promises);
+	const hatResults = hatPromises.filter(h => h.status === "fulfilled").map(h => h.value);
+	const filesSkipped = hatResults.filter(h => h.blob == null);
+	const hatListAll = hatResults.filter(h => h.blob != null);
+	const errors = hatPromises.filter(h => h.status === "rejected").map(h => h.reason);
+	const newHats = hatListAll.filter(h => h?.duplicateOf == null);
+	const hatDupeList = hatListAll.filter(h => h?.duplicateOf != null);
+	const newHatsRenamed = newHats.filter(h => h?.isRenamedToBeUnique);
+	console.log(`Finished processing all ${files.length} files, added ${newHats.length} new hats.\n`, newHats);
+	if (hatDupeList.length > 0) {
+		console.warn(`Found ${hatDupeList.length} duplicate hats that were skipped:\n`, hatDupeList);
+	}
+	if (newHatsRenamed.length > 0) {
+		console.warn(`Found ${newHatsRenamed.length} new hats that were renamed to be unique:\n`, newHatsRenamed);
+	}
+	if (filesSkipped.length > 0) {
+		console.warn(`Found ${filesSkipped.length} files that were not hat files:\n`, filesSkipped);
+	}
+	if (errors.length > 0) {
+		console.error(`Found ${errors.length} errors while processing hats:`, errors);
+		alert(`Finished processing files with ${errors.length} errors. Please check the console for details.`);
+	}
+	if (newHats.length !== hats.length) {
+		console.log(`Total hats accumulated: ${hats.length}\n`, hats);
+	}
+	await tryCreateZip(hats);
+}
+
+async function batchProcessArray(list, processFunc, batchSize = 100) {
+	const array = Array.from(list);
+	console.log(`Started processing ${array.length} elements in total.`);
+	const totalBatches = Math.ceil(array.length / batchSize);
+	const promises = [];
+	for (let start = 0; start < array.length; start += batchSize) {
+		const batch = array.slice(start, start + batchSize);
+		if (list.length > batchSize) {
+			console.log(`Started processing batch ${Math.floor(start / batchSize) + 1} of ${totalBatches}: ${batch.length} elements...`);
+		}
+		const batchPromises = batch.map(element => processFunc(element));
 		const results = await Promise.allSettled(batchPromises);
 		promises.push(...results);
 	}
-	const hatList = await Promise.all(promises);
-	console.log(hatList)
-	createZip()
-	console.log(`Finished processing all ${fileList.length} files into ${hats.length} hats.`);
-	dlAllLink.hidden = false;
+	// console.log(`Finished processing all ${array.length} elements.`);
+	return promises;
 }
 
-function getHatFileInfo(file) {
-	return new Promise((resolve, reject) => {
-		const reader = new FileReader();
-		reader.onload = (result) => {
-			decryptHat(result.target.result).then((r) => {
-				const nameSanitized = sanitizeFileName(r.name);
-				const nameSanitizedUnique = makeUniqueFileName(nameSanitized, hats.map(h => h.newFileName));
-				// "name" is the metadata inside the hat file.
-				// "hatFileName" is the original .hat file name.
-				// "newFileName" is the sanitized and made-unique name for the output PNG.
-				const h = { name: r.name, hatFileName: file.name, newFileName: nameSanitizedUnique, blob: r.blob }
-				const alreadyExistingHat = existHat(h, hats);
-				if (alreadyExistingHat) {
-					console.warn(`Hat "${h.name}" with ${h.blob.size} bytes already exists. Skipping duplicate: ${h.hatFileName} (already added from: "${alreadyExistingHat.hatFileName}")`);
-				}
-				else {
-					hats.push(h);
-					createOutputElem(h.name, h.hatFileName, h.newFileName, h.blob);
-					console.log(`Processed file: ${h.hatFileName} -> ${h.name}`);
-				}
-				resolve(h);
-			})
+function hasHatFileExtension(fileName) {
+	return fileName.toLowerCase().endsWith(".hat");
+}
+
+async function loadHatFile(file) {
+	const fileBytesBuffer = await file.arrayBuffer();
+	let decryptedHat = null;
+	try {
+		decryptedHat = await decryptHat(fileBytesBuffer);
+	}
+	catch (e) {
+		const hasHatExtension = hasHatFileExtension(file.name);
+		if (!hasHatExtension) {
+			console.warn(`File "${file.name}" is not a 'hat' file and does not have the '.hat' extension. Skipping...`);
+			return { hatFileName: file.name, message: "Not a .hat file" };
 		}
-		reader.readAsArrayBuffer(file);
-	})
+		const errorMessage = `Error processing file ${file.name} - it may not be a valid .hat file or may be corrupted.`;
+		console.error(errorMessage, e);
+		throw new Error(`${errorMessage}: ${e.message}`, { cause: e });
+	}
+	const nameSanitized = sanitizeFileName(decryptedHat.name);
+	const nameSanitizedUnique = makeUniqueFileName(nameSanitized, hats.map(h => h.newFileName));
+	const isRenamedToBeUnique = nameSanitizedUnique !== nameSanitized;
+	// "name" is the metadata inside the hat file.
+	// "hatFileName" is the original .hat file name.
+	// "newFileName" is the sanitized and made-unique name for the output PNG.
+	const hat = {
+		hatFileName: file.name,
+		name: decryptedHat.name,
+		newFileName: nameSanitizedUnique,
+		isRenamedToBeUnique: isRenamedToBeUnique,
+		blob: decryptedHat.blob
+	}
+	const alreadyExistingHat = existHat(hat, hats);
+	if (alreadyExistingHat) {
+		hat.duplicateOf = alreadyExistingHat;
+		console.warn(`Hat "${hat.name}" with ${hat.blob.size} bytes already exists. Skipping duplicate: ${hat.hatFileName} (already added from: "${alreadyExistingHat.hatFileName}")`);
+	}
+	else {
+		hats.push(hat);
+		createOutputElem(hat.name, hat.hatFileName, hat.newFileName, hat.blob);
+		console.log(`Processed file: ${hat.hatFileName} -> ${hat.newFileName}.png`);
+	}
+	return hat;
 }
 
 const inputElement = document.getElementById("upload");
-inputElement.addEventListener("change", handleFiles, false);
+inputElement.addEventListener("change", () => handleFiles(inputElement.files), false);
 
 let dropArea = document.getElementById("dropArea");
 let dropbox = document.getElementById("uploadbox");
@@ -97,57 +160,56 @@ let unnamedCounter = 0
 
 // https://github.com/penguinscode/Quackhead/blob/2681ee7b71a57ab235742a24399babfe323b3ac5/quackhead.js#L14
 const keyb64 = "8xaYIAH0em9hKg0CEw8t5g==";
-const keyBytes = Uint8Array.from(atob(keyb64), c => c.charCodeAt(0))
-let key;
-crypto.subtle.importKey(
-	"raw",
-	keyBytes,
-	{ name: "AES-CBC" },
-	false,
-	["decrypt"]
-).then(r => key = r);
+const keyBytes = Uint8Array.from(atob(keyb64), c => c.charCodeAt(0));
+let encryptionKey = null;
 
-function decryptHat(hatByteArray){
-	const ivLength = new DataView(hatByteArray.slice(0, 4)).getUint8()
-	const iv = hatByteArray.slice(4, 4+ivLength)
-	return window.crypto.subtle.decrypt(
-		{
-		  name: "AES-CBC",
-		  iv: iv
-		},
-		key,
-		hatByteArray.slice(20)
-	).then((r) => parseMetadata(r))
+async function getEncryptionKey(){
+	if (encryptionKey != null) {
+		return encryptionKey;
+	}
+	encryptionKey = await crypto.subtle.importKey(
+		"raw",
+		keyBytes,
+		{ name: "AES-CBC" },
+		false,
+		["decrypt"]
+	);
+	return encryptionKey;
 }
 
-function parseMetadata(hatByteArray) {
-	return new Promise((resolve, reject) => {
-		try{
-			const hatNameLength = new DataView(hatByteArray.slice(8, 9)).getUint8()
-			const hatName = hatByteArray.slice(9, 9+hatNameLength)
-			let t = new TextDecoder()
-			let nameDecoded = t.decode(hatName)
-	
-			const blob = new Blob([hatByteArray.slice(9+hatNameLength+4)], {
-				type: "image/png"
-			})
-			
-			const name = nameDecoded || `unnamed hat ${unnamedCounter}`
-			if(!nameDecoded) {
-				console.warn(`Hat name is empty. Using default name: ${name}`)
-				unnamedCounter++
-			}
-			const sanitizedName = sanitizeHatMetadataName(name)
-			const hatInfo = { name: sanitizedName, fileName: name, blob: blob }
-			//hats.push(hatInfo)
-			//console.log(`Hat parsed: ${sanitizedName}`)
+async function decryptHat(hatFileArrayBuffer){
+	const encryptionKey = await getEncryptionKey();
+	const ivLength = new DataView(hatFileArrayBuffer.slice(0, 4)).getUint8();
+	const iv = hatFileArrayBuffer.slice(4, 4+ivLength);
+	const decryptedHatArrayBuffer = await window.crypto.subtle.decrypt(
+		{
+			name: "AES-CBC",
+			iv: iv
+		},
+		encryptionKey,
+		hatFileArrayBuffer.slice(20)
+	);
+	return parseMetadata(decryptedHatArrayBuffer);
+}
 
-			resolve(hatInfo)
-		} catch(e) {
-			console.error(e)
-			reject(e)
-		}
+function parseMetadata(decryptedHatArrayBuffer) {
+	const hatNameLength = new DataView(decryptedHatArrayBuffer.slice(8, 9)).getUint8()
+	const hatName = decryptedHatArrayBuffer.slice(9, 9+hatNameLength)
+	let t = new TextDecoder()
+	let nameDecoded = t.decode(hatName)
+
+	const blob = new Blob([decryptedHatArrayBuffer.slice(9+hatNameLength+4)], {
+		type: "image/png"
 	})
+
+	const name = nameDecoded || `unnamed hat ${unnamedCounter}`
+	if (!nameDecoded) {
+		console.warn(`Hat name is empty. Using default name: ${name}`)
+		unnamedCounter++
+	}
+	const sanitizedName = sanitizeHatMetadataName(name)
+	const hatInfo = { name: sanitizedName, fileName: name, blob: blob }
+	return hatInfo;
 }
 
 const invalidFileNameChars = '/\\:*?"<>|';
@@ -155,10 +217,12 @@ const invalidFileNameChars = '/\\:*?"<>|';
 function makeUniqueFileName(name, existingNames) {
 	let nameUnique = name
 	let counter = 1
-	while(existingNames.some(n => n.toLowerCase() === nameUnique.toLowerCase())) {
+	while (existingNames.some(n => n.toLowerCase() === nameUnique.toLowerCase())) {
 		nameUnique = name +"_" + counter
-		console.warn(`File name "${name}" already exists. Trying "${nameUnique}"...`)
 		counter++
+	}
+	if (nameUnique !== name) {
+		// console.warn(`File name "${name}.png" is not unique. Using "${nameUnique}.png" instead.`)
 	}
 	return nameUnique
 }
@@ -189,21 +253,6 @@ function compareHats(hatInfo1, hatInfo2) {
 		&& hatInfo1.blob.size === hatInfo2.blob.size;
 }
 
-function removeHatDuplicates() {
-	const seen = new Set();
-	for(let i = 0; i < hats.length; i++) {
-		const hat = hats[i];
-		const key = `${hat.name}|${hat.blob.size}`;
-		if (!seen.has(key)) {
-			seen.add(key);
-		}
-		else {
-			hats.splice(i, 1);
-			i--;
-		}
-	}
-}
-
 function getNameDescription(name, hatFileName, newFileName) {
 	let nameDesc = name;
 	const hatFileNameNoExtension = removeHatExtension(hatFileName);
@@ -221,40 +270,49 @@ function createOutputElem(name, hatFileName, newFileName, blob){
 	const img = document.createElement("img")
 	const title = document.createElement("div")
 	const a = document.createElement("a")
-	
+	// img.title = `Download ${newFileName}.png`
+
 	cont.className = "duck-out"
-	
+
 	title.className = "title"
 	const nameDesc = getNameDescription(name, hatFileName, newFileName)
 	title.innerText = nameDesc
 	title.title = `Original file name: ${hatFileName}\nHat metadata name: ${name}\nOutput file name: ${newFileName}.png`
-	
+
 	img.src = URL.createObjectURL(blob)
 	img.alt = `Image for ${hatFileName}`
-	
+
 	a.href = img.src
 	a.download = `${newFileName}.png`
 	a.appendChild(img)
-	
+
 	cont.appendChild(title)
 	cont.appendChild(a)
 	document.getElementById("out").appendChild(cont)
 }
 
-function createZip(){
-	var zip = new JSZip();
+async function tryCreateZip(hats){
+	try {
+		await createZip(hats);
+	}
+	catch(e) {
+		console.error("Error creating ZIP file:", e);
+		alert("An error occurred while creating the ZIP file. Please check the console for details.");
+	}
+}
+
+async function createZip(hats){
+	const zip = new JSZip();
 
 	hats.forEach(hat => {
 		let fn = hat.newFileName
-		while(zip.file(`${fn}.png`)){
+		while (zip.file(`${fn}.png`)){
 			fn = `${fn}_`
 		}
 		zip.file(`${fn}.png`, hat.blob)
 	})
 
-	zip.generateAsync({type:"blob"})
-	.then(function(content) {
-		dlAllLink.href = URL.createObjectURL(content)
-		dlAllLink.download = "hats.zip"
-	});
-}
+	const content = await zip.generateAsync({type:"blob"});
+	downloadZipButton.href = URL.createObjectURL(content)
+	downloadZipButton.download = "hats.zip"
+};
